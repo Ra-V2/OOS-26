e0de051308438e8e7cb57b45f86fcc777b20defb
Rafał Bernacki <rav.bernacki@gmail.com>
2026-03-11 21:25:25 +0100

    01 Add procedure to sort numbers

--- 01/main.pas
+++ 01/main.pas
@@ -11,12 +11,32 @@ begin
         results[i] := Random(101);
 end;
 
+procedure BubbleSort(var arr: array of integer);
+var
+    i, j, temp: integer;
+begin
+    for i := Low(arr) to High(arr) - 1 do
+        for j := Low(arr) to High(arr) - i - 1 do
+            if arr[j] > arr[j + 1] then
+            begin
+                temp := arr[j];
+                arr[j] := arr[j + 1];
+                arr[j + 1] := temp;
+            end;
+end;
+
 var
     i: integer;
 begin
     Randomize;
     RandomNumbers(numbers);
     WriteLn('Random numbers:');
+    for i := 1 to 50 do
+        Write(numbers[i], ' ');
+    WriteLn;
+
+    BubbleSort(numbers);
+    WriteLn('Sorted numbers:');
     for i := 1 to 50 do
         Write(numbers[i], ' ');
     WriteLn;
