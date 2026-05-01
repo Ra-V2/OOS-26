ff355c50647e6844512b17821ca25598de7fe409
Rafał Bernacki <rav.bernacki@gmail.com>
2026-05-01 17:44:14 +0200

    05 Implement routing between Products, Cart and Payments, add cart component

--- 05/shop/client/src/App.tsx
+++ 05/shop/client/src/App.tsx
@@ -1,15 +1,26 @@
 import './App.css'
 
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Products from "./pages/Products";
 import Payments from "./pages/Payments";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
-    <div>
-      <Products />
-      <Payments />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Products</Link> |{" "}
+        <Link to="/cart">Cart</Link> |{" "}
+        <Link to="/payments">Payments</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/payments" element={<Payments />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
